fbfc4eeca83d9b3a61edb64c2e0268c18f66cb72
mathiashole <mangino@fcien.edu.uy>
2026-01-26 16:10:31 -0300

    Created new function

---
+++ process_text_directory.R
@@ -0,0 +1,37 @@
+process_text_directory <- function(input_dir, output_dir, config) {
+
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir, recursive = TRUE)
+  }
+
+  files <- list.files(input_dir, full.names = TRUE)
+
+  if (length(files) == 0) {
+    stop("No se encontraron archivos en ", input_dir)
+  }
+
+  for (file in files) {
+
+    message("→ Procesando: ", basename(file))
+
+    texto_crudo <- readLines(file, warn = FALSE)
+    # normalized encoding to UTF-8
+    texto_crudo <- iconv(texto_crudo, from = "", to = "UTF-8", sub = "")
+
+    texto_limpio <- vapply(
+      texto_crudo,
+      clean_text,
+      character(1),
+      config = config
+    )
+
+    message(mean(is.na(texto_limpio)))
+
+    writeLines(
+      texto_limpio,
+      file.path(output_dir, basename(file))
+    )
+  }
+}
+
+
