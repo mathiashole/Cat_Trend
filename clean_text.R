3b91a98a3afb12f87996f18e36e1771a84a5bd40
mathiashole <mangino@fcien.edu.uy>
2026-01-28 12:16:16 -0300

    Update normal fish

--- clean_text.R
+++ clean_text.R
@@ -3,5 +3,12 @@ normalizar_plural <- function(palabra) {
   if (is.na(palabra) || palabra == "")
     return(NA_character_)
 
+  palabra <- tolower(trimws(palabra))
+
+  # peces -> pez
+  palabra <- sub("ces$", "z", palabra)
+
+  n <- nchar(palabra)
+
 
 }
