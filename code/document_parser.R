b9cb206174c43f9451a852e509f36aa92e5157a2
mathiashole <mangino@fcien.edu.uy>
2026-05-24 16:10:32 -0300

    Update string as factor

--- code/document_parser.R
+++ code/document_parser.R
@@ -12,7 +12,7 @@ parse_documents_to_paragraphs <- function(directory, data_table_path) {
     }
 
     # Read the metadata table with document paths and dates
-    data_table <- read.table(data_table_path, sep = "\t", header = FALSE, col.names = c("document", "date"))
+    data_table <- read.table(data_table_path, sep = "\t", header = FALSE, col.names = c("document", "date"), stringsAsFactors = FALSE)
     data_table$date <- gsub("/", "-", data_table$date)
 
     # Normaliztion of document names with clean.R functions
