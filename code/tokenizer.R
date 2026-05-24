b407126eebf9eb282a7e2c01d4229747329ddbde
mathiashole <mangino@fcien.edu.uy>
2026-05-24 16:37:27 -0300

    Update charge stopwords EN

--- code/tokenizer.R
+++ code/tokenizer.R
@@ -10,6 +10,8 @@ tokenize_corpus <- function(paragraphs_df, language, ngram_number) {
 # Charge stopwords list based on the language
 if (tolower(language) == "sp") {
     stopwords_list <- stopwords(language = "es", source = "stopwords-iso")
+}  else if (tolower(language) == "en") {
+    stopwords_list <- stopwords(language = "en", source = "stopwords-iso")
 }
 
 }
