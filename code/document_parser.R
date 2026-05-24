37f799c33cf102f3fceb609b9b0b1fc704ba5060
mathiashole <mangino@fcien.edu.uy>
2026-05-23 23:14:52 -0300

    Update read meta

--- code/document_parser.R
+++ code/document_parser.R
@@ -11,6 +11,10 @@ parse_documents_to_paragraphs <- function(directory, data_table_path) {
         stop(paste("ERROR [Parser]: No se encuentra la tabla de metadatos en:", data_table_path))
     }
 
+    # Read the metadata table with document paths and dates
+    data_table <- read.table(data_table_path, sep = "\t", header = FALSE, col.names = c("document", "date"))
+    data_table$date <- gsub("/", "-", data_table$date)
+
 
 
     return(infoText)
