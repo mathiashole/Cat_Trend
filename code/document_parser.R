b96edf8cbd7de3a1d7492edfb5cc37eb3f2db556
mathiashole <mangino@fcien.edu.uy>
2026-05-23 23:14:21 -0300

    Update new function parse documents to paragraphs

--- code/document_parser.R
+++ code/document_parser.R
@@ -4,3 +4,14 @@ suppressPackageStartupMessages({
     library(dplyr)
     library(tibble)
 })
+
+parse_documents_to_paragraphs <- function(directory, data_table_path) {
+
+    if (!file.exists(data_table_path)) {
+        stop(paste("ERROR [Parser]: No se encuentra la tabla de metadatos en:", data_table_path))
+    }
+
+
+
+    return(infoText)
+}
