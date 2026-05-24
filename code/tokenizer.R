72d44247657b58d54c480975799a5327d0385226
mathiashole <mangino@fcien.edu.uy>
2026-05-24 16:36:49 -0300

    Update charge stopwords

--- code/tokenizer.R
+++ code/tokenizer.R
@@ -6,4 +6,10 @@ suppressPackageStartupMessages({
     library(stopwords)
 })
 
-tokenize_corpus <- function(paragraphs_df, language, ngram_number) {}
+tokenize_corpus <- function(paragraphs_df, language, ngram_number) {
+# Charge stopwords list based on the language
+if (tolower(language) == "sp") {
+    stopwords_list <- stopwords(language = "es", source = "stopwords-iso")
+}
+
+}
