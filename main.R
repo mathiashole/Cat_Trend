849ca9c3b00432dca84c1575166d056dab5dec2a
mathiashole <mangino@fcien.edu.uy>
2026-05-25 10:05:07 -0300

    Update call modules

--- main.R
+++ main.R
@@ -4,4 +4,18 @@ suppressPackageStartupMessages({
     library(yaml)
 })
 
+# ------------------------------ Load modules ------------------------------
+
+source("code/init_utilitis.R")
+
+# Core pipeline
+source("code/get_time_v1.R")
+source("code/document_parser.R")
+source("code/clean_text.R")
+source("code/tokenizer.R")
+source("code/txt_ngram_to_tidy.R")
+
+# Analysis
+source("code/txt_frequency.R")
+
 
