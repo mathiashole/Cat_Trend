71577ab8a09e38f4383aa03bea52855e78f909cb
mathiashole <mangino@fcien.edu.uy>
2026-05-26 10:48:15 -0300

    Update metadata file

--- code/txt_ngram_to_tidy.R
+++ code/txt_ngram_to_tidy.R
@@ -8,4 +8,6 @@ source("code/tokenizer.R")
 main_process_texts <- function(directory, language, ngram_number, date_hour, output_dir, custom_stopwords = NULL) {
   cat("[1/3] Parsing documents into structured tables...\n")
 
+    metadata_file <- file.path(output_dir, paste0("data_table_", date_hour, ".txt"))
+
 }
