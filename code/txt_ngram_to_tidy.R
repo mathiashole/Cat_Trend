6fcbf91611c432a9ed2bdf7405933c263f444fb9
mathiashole <mangino@fcien.edu.uy>
2026-05-27 22:15:15 -0300

    Update step A parse document

--- code/txt_ngram_to_tidy.R
+++ code/txt_ngram_to_tidy.R
@@ -11,6 +11,7 @@ main_process_texts <- function(directory, language, ngram_number, date_hour, out
     metadata_file <- file.path(output_dir, paste0("data_table_", date_hour, ".txt"))
 
     # Step A: Generate structured paragraph dataframe
+    paragraphs_df <- parse_documents_to_paragraphs(directory, metadata_file)
 
 }
 
