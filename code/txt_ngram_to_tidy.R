190fc823fac1079fac518a63e6f5a9646d23d36a
mathiashole <mangino@fcien.edu.uy>
2026-05-27 22:15:48 -0300

    Update save paragraph df

--- code/txt_ngram_to_tidy.R
+++ code/txt_ngram_to_tidy.R
@@ -13,6 +13,8 @@ main_process_texts <- function(directory, language, ngram_number, date_hour, out
     # Step A: Generate structured paragraph dataframe
     paragraphs_df <- parse_documents_to_paragraphs(directory, metadata_file)
 
+    # Save intermediate paragraph structure
+
 }
 
 # Other function
