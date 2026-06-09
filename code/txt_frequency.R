3ba3e3f567200360bfa11a5c0c04cb5c6e0ace30
Mathias Mangino <94571367+mathiashole@users.noreply.github.com>
2026-06-09 12:14:22 -0300

    Update plot frequency of words by documents

--- code/txt_frequency.R
+++ code/txt_frequency.R
@@ -33,8 +33,17 @@ analyze_frequency <- function(tokens_df, output_dir, plots_dir) {
     ungroup()
 
   # ------------------------------ Plotting Section ------------------------------
-  
+
   cat("  → [2/3] Generating frequency and TF-IDF visualizations...\n")
+
+  # Plot Top 10 of words (Fixed typos in filenames)
+  top_10_plot <- plot_top_10(
+    top_words_by_doc, 
+    title = "Top 10 Most Frequent Words by Document", 
+    xlab = "Words", 
+    ylab = "Frequency"
+  )
+
 
 }
 
