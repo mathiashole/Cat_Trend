0c469429a853cc04de133b96aae1e7cf87ea8744
mathiashole <mangino@fcien.edu.uy>
2026-06-10 16:24:34 -0300

    Update save df in two line

--- code/txt_frequency.R
+++ code/txt_frequency.R
@@ -123,6 +123,9 @@ analyze_frequency <- function(tokens_df, output_dir, plots_dir) {
   write.table(docs_words, file = file.path(output_dir, "documents_words_metrics.tsv"),
     row.names = FALSE, sep = "\t", quote = FALSE)
 
+  write.table(top_words_by_doc, file = file.path(output_dir, "top_words_by_document.tsv"),
+    row.names = FALSE, sep = "\t", quote = FALSE)
+
   # Return the data frames for potential further use
   return(invisible(list(
     docs_words = docs_words,
