c8a3953a1ec29a74f61065e0791276a9b904b95b
mathiashole <mangino@fcien.edu.uy>
2026-06-18 10:59:16 -0300

    Update site by tf idf

--- code/txt_frequency.R
+++ code/txt_frequency.R
@@ -88,7 +88,8 @@ analyze_frequency <- function(tokens_df, output_dir, plots_dir) {
   # Calculate TF-IDF and plot
   # docs_words <- docs_words %>%
   #   bind_tf_idf(word, document, n)
-  
+  docs_words <- docs_words %>%
+    bind_tf_idf(word, site, n)
     
   tf_idf_plot <- plot_tf_idf(
     docs_words, 
