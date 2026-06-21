dd50a842cdc132093bac0395525baab7105842b3
mathiashole <mangino@fcien.edu.uy>
2026-06-21 18:44:15 -0300

    Update words by site

--- code/txt_frequency.R
+++ code/txt_frequency.R
@@ -47,6 +47,9 @@ analyze_frequency <- function(tokens_df, output_dir, plots_dir) {
   # docs_words <- left_join(docs_words, total_words, by = "document")
   docs_words <- left_join(docs_words, total_words, by = "document")
 
+  docs_words_site <- tokens_df %>%
+    count(site, word, sort = TRUE)
+
   #3.Top 10 words by document (Fixing deprecated top_n -> slice_max)
   # top_words_by_doc <- docs_words %>%
   #   group_by(document) %>%
